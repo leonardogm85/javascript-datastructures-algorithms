e19b7a59de992a63f6f8f18b374f726cc68ea9a4
Leonardo Gomes Martins <leonardo.gomes@anm.gov.br>
2025-05-07 14:09:20 -0300

    update binary search tree tests

--- tests/data-structures/binary-search-tree.spec.ts
+++ tests/data-structures/binary-search-tree.spec.ts
@@ -2,7 +2,32 @@ import { BinarySearchTree } from "../../src/data-structures/binary-search-tree";
 import { Node } from "../../src/models/tree-model";
 
 describe('BinarySearchTree', () => {
-  function assertNode(node: Node<number> | undefined, key: number | undefined, left: number | undefined, right: number | undefined): void {
+  function insertsElements(tree: BinarySearchTree<number>): void {
+    //  n0 ->              11
+    //                 /        \
+    //  n1 ->       7             15
+    //            /   \         /   \
+    //  n2 ->    5     9       13    20
+    //          /     / \     / \    / \
+    //  n3 ->  3     8   10  12 14  18 25
+
+    tree.insert(11);
+    tree.insert(7);
+    tree.insert(15);
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(9);
+    tree.insert(8);
+    tree.insert(10);
+    tree.insert(13);
+    tree.insert(12);
+    tree.insert(14);
+    tree.insert(20);
+    tree.insert(18);
+    tree.insert(25);
+  }
+
+  function verifyNode(node: Node<number> | undefined, key: number | undefined, left: number | undefined, right: number | undefined): void {
     if (key) {
       expect(node?.key).toEqual(key);
     } else {
@@ -28,80 +53,67 @@ describe('BinarySearchTree', () => {
     expect(tree.getRoot()).toBeUndefined();
   });
 
-  it('inserts elements in the BST', () => {
+  it('inserts elements', () => {
     const tree: BinarySearchTree<number> = new BinarySearchTree<number>();
 
     expect(tree.getRoot()).toBeUndefined();
 
-    //              11
-    //        /           \
-    //       7             15
-    //     /   \         /   \
-    //    5     9       13    20
-    //   /     / \     / \    / \
-    //  3     8   10  12 14  18 25
-
-    tree.insert(11);
-    tree.insert(7);
-    tree.insert(15);
-    tree.insert(5);
-    tree.insert(3);
-    tree.insert(9);
-    tree.insert(8);
-    tree.insert(10);
-    tree.insert(13);
-    tree.insert(12);
-    tree.insert(14);
-    tree.insert(20);
-    tree.insert(18);
-    tree.insert(25);
+    insertsElements(tree);
 
     let node: Node<number> | undefined;
 
-    node = tree.getRoot();
-    assertNode(node, 11, 7, 15);
+    //  n0 ->              11
+    //                 /        \
+    //  n1 ->       7             15
+    //            /   \         /   \
+    //  n2 ->    5     9       13    20
+    //          /     / \     / \    / \
+    //  n3 ->  3     8   10  12 14  18 25
 
-    node = node?.left;
-    assertNode(node, 7, 5, 9);
+    node = tree.getRoot(); // n0
+    verifyNode(node, 11, 7, 15);
 
-    node = node?.left;
-    assertNode(node, 5, 3, undefined);
+    node = tree.getRoot()?.left; // n1
+    verifyNode(node, 7, 5, 9);
 
-    node = node?.left;
-    assertNode(node, 3, undefined, undefined);
+    node = tree.getRoot()?.left?.left; // n2
+    verifyNode(node, 5, 3, undefined);
 
-    node = tree.getRoot()?.left?.left?.right;
-    assertNode(node, undefined, undefined, undefined);
+    node = tree.getRoot()?.left?.left?.left; // n3
+    verifyNode(node, 3, undefined, undefined);
 
-    node = tree.getRoot()?.left?.right;
-    assertNode(node, 9, 8, 10);
+    node = tree.getRoot()?.left?.left?.right; // n3
+    verifyNode(node, undefined, undefined, undefined);
 
-    node = node?.left;
-    assertNode(node, 8, undefined, undefined);
+    node = tree.getRoot()?.left?.right; // n2
+    verifyNode(node, 9, 8, 10);
 
-    node = tree.getRoot()?.left?.right?.right;
-    assertNode(node, 10, undefined, undefined);
+    node = tree.getRoot()?.left?.right?.left; // n3
+    verifyNode(node, 8, undefined, undefined);
 
-    node = tree.getRoot()?.right;
-    assertNode(node, 15, 13, 20);
+    node = tree.getRoot()?.left?.right?.right; // n3
+    verifyNode(node, 10, undefined, undefined);
 
-    node = node?.left;
-    assertNode(node, 13, 12, 14);
+    node = tree.getRoot()?.right; // n1
+    verifyNode(node, 15, 13, 20);
 
-    node = node?.left;
-    assertNode(node, 12, undefined, undefined);
+    node = tree.getRoot()?.right?.left; // n2
+    verifyNode(node, 13, 12, 14);
 
-    node = tree.getRoot()?.right?.left?.right;
-    assertNode(node, 14, undefined, undefined);
+    node = tree.getRoot()?.right?.left?.left; // n3
+    verifyNode(node, 12, undefined, undefined);
 
-    node = tree.getRoot()?.right?.right;
-    assertNode(node, 20, 18, 25);
+    node = tree.getRoot()?.right?.left?.right; // n3
+    verifyNode(node, 14, undefined, undefined);
 
-    node = node?.left;
-    assertNode(node, 18, undefined, undefined);
+    node = tree.getRoot()?.right?.right; // n2
+    verifyNode(node, 20, 18, 25);
 
-    node = tree.getRoot()?.right?.right?.right;
-    assertNode(node, 25, undefined, undefined);
+    node = tree.getRoot()?.right?.right?.left; // n3
+    verifyNode(node, 18, undefined, undefined);
+
+    node = tree.getRoot()?.right?.right?.right; // n3
+    verifyNode(node, 25, undefined, undefined);
   });
 
   it('verifies if element exists', () => {
