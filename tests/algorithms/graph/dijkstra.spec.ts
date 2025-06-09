7f1f60df1860e1f6827b4f660f83c15c504ac26f
Leonardo Gomes Martins <leonardo.gomes@anm.gov.br>
2025-06-09 17:46:57 -0300

    update dijkstra and floyd warshall

--- tests/algorithms/graph/dijkstra.spec.ts
+++ tests/algorithms/graph/dijkstra.spec.ts
@@ -2,16 +2,7 @@ import { dijkstra } from '../../../src/algorithms/graph/dijkstra';
 
 describe('Algorithm of Dijkstra', () => {
   it('Shortest Path', () => {
-
-    //     A B C D E F
-    //  A  0 2 4 0 0 0
-    //  B  0 0 2 4 2 0
-    //  C  0 0 0 0 3 0
-    //  D  0 0 0 0 0 2
-    //  E  0 0 0 3 0 2
-    //  F  0 0 0 0 0 0
-
-    // Source: A -> Shortest Path
+    let graph: number[][] | undefined;
 
     //  Vertex  Distance
     //  A       0 (A)
@@ -21,15 +12,60 @@ describe('Algorithm of Dijkstra', () => {
     //  E       4 (A, B, E)
     //  F       6 (A, B, E, F)
 
-    const graph: number[][] = [
-      [0, 2, 4, 0, 0, 0],
-      [0, 0, 2, 4, 2, 0],
-      [0, 0, 0, 0, 3, 0],
-      [0, 0, 0, 0, 0, 2],
-      [0, 0, 0, 3, 0, 2],
-      [0, 0, 0, 0, 0, 0]
+    // A  B  C  D  E  F
+    graph = [
+      [0, 2, 4, 0, 0, 0], //  A
+      [0, 0, 2, 4, 2, 0], //  B
+      [0, 0, 0, 0, 3, 0], //  C
+      [0, 0, 0, 0, 0, 2], //  D
+      [0, 0, 0, 3, 0, 2], //  E
+      [0, 0, 0, 0, 0, 0]  //  F
     ];
 
     expect(dijkstra(graph, 0)).toEqual([0, 2, 4, 6, 4, 6]);
+
+    //        A
+    //    1↙    ↘8
+    //  D  4↑  2→  B
+    //    9↘    ↙1
+    //        C
+
+    //  Vertex  Distance
+    //  A       0 (A)
+    //  B       3 (A, D, B)
+    //  C       4 (A, D, B, C)
+    //  D       1 (A, D)
+
+    // A  B  C  D
+    graph = [
+      [0, 8, 0, 1], //  A
+      [0, 0, 1, 0], //  B
+      [4, 0, 0, 0], //  C
+      [0, 2, 9, 0]  //  D
+    ];
+
+    expect(dijkstra(graph, 0)).toEqual([0, 3, 4, 1]);
+
+    //       B
+    //    3↗  ↘5
+    //  A   8←   D
+    //    4↘  ↗3
+    //       C
+
+    //  Vertex  Distance
+    //  A       0 (A)
+    //  B       3 (A, B)
+    //  C       4 (A, C)
+    //  D       7 (A, C, D)
+
+    // A  B  C  D
+    graph = [
+      [0, 3, 4, 0], //  A
+      [0, 0, 0, 5], //  B
+      [0, 0, 0, 3], //  C
+      [8, 0, 0, 0]  //  D
+    ];
+
+    expect(dijkstra(graph, 0)).toEqual([0, 3, 4, 7]);
   });
 });
--- tests/algorithms/graph/floyd-warshall.spec.ts
+++ tests/algorithms/graph/floyd-warshall.spec.ts
@@ -6,49 +6,49 @@ describe('Algorithm of Floyd-Warshall', () => {
 
     let graph: number[][] | undefined;
 
-    // Source Vertex  Distance
+    // Path  Distance
     //
-    // A      A       0 (A)
-    // A      B       2 (A, B)
-    // A      C       4 (A, C) OU (A, B, C)
-    // A      D       6 (A, B, D)
-    // A      E       4 (A, B, E)
-    // A      F       6 (A, B, E, F)
+    // A-A   0 (A)
+    // A-B   2 (A, B)
+    // A-C   4 (A, C) OU (A, B, C)
+    // A-D   6 (A, B, D)
+    // A-E   4 (A, B, E)
+    // A-F   6 (A, B, E, F)
     //
-    // B      A       I
-    // B      B       0 (B)
-    // B      C       2 (B, C)
-    // B      D       4 (B, D)
-    // B      E       2 (B, E)
-    // B      F       4 (B, E, F)
+    // B-A   I
+    // B-B   0 (B)
+    // B-C   2 (B, C)
+    // B-D   4 (B, D)
+    // B-E   2 (B, E)
+    // B-F   4 (B, E, F)
     //
-    // C      A       I
-    // C      B       I
-    // C      C       0 (C)
-    // C      D       6 (C, E, D)
-    // C      E       3 (C, E)
-    // C      F       5 (C, E, F)
+    // C-A   I
+    // C-B   I
+    // C-C   0 (C)
+    // C-D   6 (C, E, D)
+    // C-E   3 (C, E)
+    // C-F   5 (C, E, F)
     //
-    // D      A       I
-    // D      B       I
-    // D      C       I
-    // D      D       0 (D)
-    // D      E       I
-    // D      F       2 (D, F)
+    // D-A   I
+    // D-B   I
+    // D-C   I
+    // D-D   0 (D)
+    // D-E   I
+    // D-F   2 (D, F)
     //
-    // E      A       I
-    // E      B       I
-    // E      C       I
-    // E      D       3 (E, D)
-    // E      E       0 (E)
-    // E      F       2 (E, F)
+    // E-A   I
+    // E-B   I
+    // E-C   I
+    // E-D   3 (E, D)
+    // E-E   0 (E)
+    // E-F   2 (E, F)
     //
-    // F      A       I
-    // F      B       I
-    // F      C       I
-    // F      D       I
-    // F      E       I
-    // F      F       0 (F)
+    // F-A   I
+    // F-B   I
+    // F-C   I
+    // F-D   I
+    // F-E   I
+    // F-F   0 (F)
 
     //                       *                   *                   *                   *                   *                   *
     //     A B C D E F       A B C D E F       A B C D E F       A B C D E F       A B C D E F       A B C D E F       A B C D E F
@@ -83,34 +83,34 @@ describe('Algorithm of Floyd-Warshall', () => {
     //    9↘    ↙1
     //        C
 
-    // Source Vertex  Distance
+    // Path  Distance
     //
-    // A      A       0 (A)
-    // A      B       3 (A, D, B)
-    // A      C       4 (A, D, B, C)
-    // A      D       1 (A, D)
+    // A-A   0 (A)
+    // A-B   3 (A, D, B)
+    // A-C   4 (A, D, B, C)
+    // A-D   1 (A, D)
     //
-    // B      A       5 (B, C, A)
-    // B      B       0 (B)
-    // B      C       1 (B, C)
-    // B      D       6 (B, C, A, D)
+    // B-A   5 (B, C, A)
+    // B-B   0 (B)
+    // B-C   1 (B, C)
+    // B-D   6 (B, C, A, D)
     //
-    // C      A       4 (C, A)
-    // C      B       7 (C, A, D, B)
-    // C      C       0 (C)
-    // C      D       5 (C, A, D)
+    // C-A   4 (C, A)
+    // C-B   7 (C, A, D, B)
+    // C-C   0 (C)
+    // C-D   5 (C, A, D)
     //
-    // D      A       7 (D, B, C, A)
-    // D      B       2 (D, B)
-    // D      C       0 (D, B, C)
-    // D      D       0 (D)
+    // D-A   7 (D, B, C, A)
+    // D-B   2 (D, B)
+    // D-C   0 (D, B, C)
+    // D-D   0 (D)
 
-    //                  *                 *                *               *
-    //     A B C D      A  B C D       A  B C D       A  B C D       A B C D
-    //  A  0 8 i 1   A  0  8 i 1    A  0  8 9 1    A  0  8 9 1    A  0 3 4 1
-    //  B  i 0 1 i   B  i  0 1 i    B  i  0 1 i    B  5  0 1 6    B  5 0 1 6
-    //  C  4 i 0 i   C  4 12 0 5    C  4 12 0 5    C  4 12 0 5    C  4 7 0 5
-    //  D  i 2 9 0   D  i  2 9 0    D  i  2 3 0    D  7  2 3 0    D  7 2 3 0
+    //                   *                 *                *               *
+    //     A B C D       A  B C D       A  B C D       A  B C D       A B C D
+    //  A  0 8 i 1    A  0  8 i 1    A  0  8 9 1    A  0  8 9 1    A  0 3 4 1
+    //  B  i 0 1 i    B  i  0 1 i    B  i  0 1 i    B  5  0 1 6    B  5 0 1 6
+    //  C  4 i 0 i    C  4 12 0 5    C  4 12 0 5    C  4 12 0 5    C  4 7 0 5
+    //  D  i 2 9 0    D  i  2 9 0    D  i  2 3 0    D  7  2 3 0    D  7 2 3 0
 
     graph = [
       [i, 8, i, 1],
@@ -132,27 +132,27 @@ describe('Algorithm of Floyd-Warshall', () => {
     //    4↘  ↗3
     //       C
 
-    // Source Vertex  Distance
+    // Path  Distance
     //
-    // A      A       0 (A)
-    // A      B       3 (A, B)
-    // A      C       4 (A, C)
-    // A      D       7 (A, C, D)
+    // A-A    0 (A)
+    // A-B    3 (A, B)
+    // A-C    4 (A, C)
+    // A-D    7 (A, C, D)
     //
-    // B      A       13 (B, D, A)
-    // B      B        0 (B)
-    // B      C       17 (B, D, A, C)
-    // B      D        5 (B, D)
+    // B-A   13 (B, D, A)
+    // B-B    0 (B)
+    // B-C   17 (B, D, A, C)
+    // B-D    5 (B, D)
     //
-    // C      A       11 (C, D, A)
-    // C      B       14 (C, D, A, B)
-    // C      C        0 (C)
-    // C      D        3 (C, D)
+    // C-A   11 (C, D, A)
+    // C-B   14 (C, D, A, B)
+    // C-C    0 (C)
+    // C-D    3 (C, D)
     //
-    // D      A        8 (D, A)
-    // D      B       11 (D, A, B)
-    // D      C       12 (D, A, C)
-    // D      D        0 (D)
+    // D-A    8 (D, A)
+    // D-B   11 (D, A, B)
+    // D-C   12 (D, A, C)
+    // D-D    0 (D)
 
     //                   *                  *                  *                  *
     //     A B C D       A  B  C D       A  B  C D       A  B  C D        A  B  C D
