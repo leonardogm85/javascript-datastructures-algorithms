1b4c13960ad0354a58580aa646a488c762194520
Leonardo Gomes Martins <leonardo.gomes@anm.gov.br>
2025-06-11 10:49:10 -0300

    add selection sort

---
+++ src/algorithms/sorting/selection-sort.ts
@@ -0,0 +1,19 @@
+import { Compare, CompareFunction, defaultCompare, swap } from "../../util";
+
+export function selectionSort<T>(array: T[], compareFn: CompareFunction<T> = defaultCompare): T[] {
+  for (let i: number = 0; i < array.length - 1; i++) {
+    let indexMin: number = i;
+
+    for (let j: number = i; j < array.length; j++) {
+      if (compareFn(array[indexMin], array[j]) === Compare.BIGGER_THAN) {
+        indexMin = j;
+      }
+    }
+
+    if (i !== indexMin) {
+      swap(array, i, indexMin);
+    }
+  }
+
+  return array;
+};
---
+++ tests/algorithms/sorting/selection-sort.spec.ts
@@ -0,0 +1,4 @@
+import { selectionSort } from "../../../src/algorithms/sorting/selection-sort";
+import { sortAlgorithmTests } from "./sort-algorithm-tests";
+
+sortAlgorithmTests(selectionSort, 'Selection Sort');
